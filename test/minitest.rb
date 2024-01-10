8acbfb740174cef726ed94e42c86261096671167
Zama15 <alexavdstiles@gmail.com>
2024-01-10 00:52:52 -0600

    Modified minitest.rb
    The unique changed is based on the classes modified in the previus
    commits. The scenaries are the same as the last version

--- test/minitest.rb
+++ test/minitest.rb
@@ -1,51 +1,52 @@
 # frozen_string_literal: false
 
 require 'minitest/autorun'
-require_relative '../src/frame'
 require_relative '../src/score'
 
 class TestFrame < Minitest::Test
   def test_is_spare
-    obj_frame = Frame.new
-    obj_score = Score.new(obj_frame)
+    obj_score = Score.new
 
-    obj_frame.roll(3)
-    obj_frame.next_turn
-    obj_frame.roll(7)
+    obj_score.play_roll(3)
+    obj_score.next_turn
+    obj_score.play_roll(7)
 
-    assert_equal(true, obj_frame.is_strike_or_spare)
+    assert_equal(true, obj_score.is_strike_or_spare)
   end
 
   def test_is_strike
-    obj_frame = Frame.new
-    obj_score = Score.new(obj_frame)
+    obj_score = Score.new
 
-    obj_frame.roll(10)
+    obj_score.play_roll(10)
 
-    assert_equal(true, obj_frame.is_strike_or_spare)
+    assert_equal(true, obj_score.is_strike_or_spare)
   end
 
   def test_calc_score_regular_shot
-    obj_frame = Frame.new
-    obj_score = Score.new(obj_frame)
+    obj_score = Score.new
 
-    obj_frame.roll(3)
-    obj_frame.next_turn
-    obj_frame.roll(4)
+    obj_score.play_roll(3)
+    obj_score.next_turn
+    obj_score.play_roll(4)
+    obj_score.finish_rolls
 
     assert_equal(7, obj_score.calculate_score)
   end
 
   def test_calc_score_spare
-    obj_frame = Frame.new
-    obj_score = Score.new(obj_frame)
+    obj_score = Score.new
 
-    obj_frame.roll(3)
-    obj_frame.next_turn
-    obj_frame.roll(7)
-    obj_frame.next_frame
-    obj_frame.roll(4)
+    obj_score.play_roll(3)
+    obj_score.next_turn
+    obj_score.play_roll(7)
+    obj_score.finish_rolls
 
-    assert_equal(14, obj_score.calculate_score)
+    obj_score.next_frame
+    obj_score.play_roll(4)
+    obj_score.next_turn
+    obj_score.play_roll(2)
+    obj_score.finish_rolls
+
+    assert_equal(14, obj_score.calculate_score_spare)
   end
 end
