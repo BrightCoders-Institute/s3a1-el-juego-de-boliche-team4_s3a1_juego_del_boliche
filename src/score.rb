816a288bb1e6f562d42cd5e404342cbd6aef13a2
Zama15 <alexavdstiles@gmail.com>
2024-01-10 23:15:19 -0600

    Finish score.rb
    added calculated_score_last_frame, modify finish_rolls to set the bonus
    rolls if the last frame, constructor initialize instance variable
    @score, all calculate relate method used now the score to increse the
    score base on what has been achieved so far
    
    Co-authored-by: etormasa <hector.meza.141@gmail.com>
    Co-authored-by: AnaCarrizales <acarrizales2@ucol.mx>

--- src/score.rb
+++ src/score.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: false
+
 require_relative 'frame'
 
 # Score class
 class Score < Frame
-  attr_accessor :matrix
+  attr_accessor :matrix, :score, :count
 
   def initialize
     super()
+    @score = 0
     @matrix = Array.new(9) { Array.new(3, 0) }
     @matrix.push(Array.new(4, 0))
   end
@@ -14,23 +16,37 @@ class Score < Frame
   def finish_rolls
     @matrix[frame][0] = rolls[0]
     @matrix[frame][1] = rolls[1]
+    @matrix[frame][2] = rolls[2] if frame == 9
   end
 
   def calculate_score
-    @matrix[frame][2] = @matrix[frame][0] + @matrix[frame][1]
+    @score += @matrix[frame][0] + @matrix[frame][1]
+    @matrix[frame][2] = @score
   end
 
   def calculate_score_spare
-    @matrix[frame - 1][2] = (@matrix[frame - 1][0] + @matrix[frame - 1][1]) + @matrix[frame][0]
+    @score += @matrix[frame - 1][0] + @matrix[frame - 1][1] + @matrix[frame][0]
+    @matrix[frame - 1][2] = @score
   end
 
-  def calculate_score_strike(option = FALSE)
-    if option == TRUE # double strike
+  def calculate_score_strike(option = false)
+    if option == true # double strike
       additional = @matrix[frame - 1][0] + @matrix[frame][0]
-      @matrix[frame - 2][2] = @matrix[frame - 2][0] + @matrix[frame - 2][1] + additional
+      @score += @matrix[frame - 2][0] + @matrix[frame - 2][1] + additional
+      @matrix[frame - 2][2] = @score
     else
       additional = @matrix[frame][0] + @matrix[frame][1]
-      @matrix[frame - 1][2] = @matrix[frame - 1][0] + @matrix[frame - 1][1] + additional
+      @score += @matrix[frame - 1][0] + @matrix[frame - 1][1] + additional
+      @matrix[frame - 1][2] = @score
+    end
+  end
+
+  def calculate_score_last_frame
+    if strike? || spare?
+      @score += @matrix[frame][0] + @matrix[frame][1] + @matrix[frame][2]
+      @matrix[frame][3] = @score
+    else
+      calculate_score
     end
   end
 end
